d342995005bdbf9d5058ccf995bb7a9be2239590
MentatRadnor <kholosinc@gmail.com>
2020-06-12 16:10:00 +0300

    igloo website, have a bag arrow

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+
+    $('.reviews__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 2000,
+        autoplay: true,
+    });
+
+});
+
+$(document).ready(function() {
+
+
+    $('.gallery__slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            }, {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+
+
+        ],
+    });
+
+});
