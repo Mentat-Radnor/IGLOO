db374fe00a6de327a365fcaedc928b8950f0290c
MentatRadnor <kholosinc@gmail.com>
2020-06-13 16:19:33 +0300

    Js (fixed header, scroll, popup slider)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,56 @@
 $(document).ready(function() {
 
+    // FIXED HEADER
+    $(function() {
+        let header = $(`#header`);
+        let intro = $(`#intro`);
+        let introH = intro.innerHeight();
+        let scrollPos = $(window).scrollTop();
+        let nav = $(`#nav`);
+        let navToggle = $(`#navToggle`);
+
+        checkScroll(scrollPos, introH);
+
+        $(window).on("scroll resize", function() {
+            let intoH = intro.innerHeight();
+            scrollPos = $(this).scrollTop();
+
+            checkScroll(scrollPos, introH);
+        });
+
+        function checkScroll(scrollPos, introH) {
+            if (scrollPos > introH) {
+                header.addClass(`fixed`);
+            } else {
+                header.removeClass(`fixed`);
+            }
+        }
+
+        // SCROLL
+        $(`[data-scroll]`).on(`click`, function(event) {
+            event.preventDefault();
+
+            let elementId = $(this).data(`scroll`);
+            let elementOffset = $(elementId).offset().top;
+
+            nav.removeClass(`nav__show`);
+
+            $(`html, body`).animate({
+                scrollTop: elementOffset - 80
+            }, 800)
+        });
+
+        // navToggle
+
+        navToggle.on(`click`, function(event) {
+            nav.toggleClass(`nav__show`)
+        })
+    });
+
+    // BURGER__menu
+
+
+
 
     $('.reviews__slider').slick({
         infinite: true,
@@ -9,10 +60,42 @@ $(document).ready(function() {
         autoplay: true,
     });
 
+
 });
 
+//  SLIDER GALLERY
 $(document).ready(function() {
+    $('.popup-link').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        mainClass: 'mfp-img-mobile',
+        image: {
+            verticalFit: true
+        }
 
+    });
+    $('.popup-link').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        image: {
+            verticalFit: false
+        }
+    });
+
+    $('.popup-link').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        fixedContentPos: true,
+        mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+        image: {
+            verticalFit: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300 // don't foget to change the duration also in CSS
+        }
+    });
 
     $('.gallery__slider').slick({
         infinite: true,
@@ -37,3 +120,5 @@ $(document).ready(function() {
     });
 
 });
+
+// POPUP IMG GALLERY
