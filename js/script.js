acdc9156d7b5244772e52ebea40b2b766d912866
MentatRadnor <kholosinc@gmail.com>
2020-06-13 18:22:49 +0300

    Фавикон, шрифты

--- js/script.js
+++ js/script.js
@@ -119,6 +119,21 @@ $(document).ready(function() {
         ],
     });
 
-});
+    // Отправка формы
+    $(".form").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            alert("Thank you!");
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
 
-// POPUP IMG GALLERY
+});
